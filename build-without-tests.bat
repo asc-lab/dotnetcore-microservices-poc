ffd049bcbd316ce481e8621fbea09e04967e787a
Robert Witkowski <robert.witkowski@altkom.pl>
2018-11-23 15:35:45 +0100

    Update .gitignore. Add script to build all projects.

---
+++ build-without-tests.bat
@@ -0,0 +1,9 @@
+call dotnet build ./ProductService.Api
+call dotnet build ./PricingService.Api
+call dotnet build ./PolicyService.Api
+call dotnet build ./PaymentService.Api
+
+call dotnet build ./ProductService
+call dotnet build ./PricingService
+call dotnet build ./PolicyService
+call dotnet build ./PaymentService
