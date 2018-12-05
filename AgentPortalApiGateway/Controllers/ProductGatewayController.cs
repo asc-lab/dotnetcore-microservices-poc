3ad9d384cbaed53908108d0a352cccce32360396
Robert Witkowski <robert.witkowski@altkom.pl>
2018-12-05 15:00:26 +0100

    Add initial AgentPortalApiGateway project with client using Polly & RestEase.

---
+++ AgentPortalApiGateway/Controllers/ProductGatewayController.cs
@@ -0,0 +1,27 @@
+﻿using AgentPortalApiGateway.RestClients;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AgentPortalApiGateway.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductGatewayController : ControllerBase
+    {
+        private readonly IProductClient client;
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var result = await client.GetAll();
+            return new JsonResult(result);
+        }
+
+        [HttpGet("{code}")]
+        public async Task<ActionResult> GetByCode([FromRoute] string code)
+        {
+            var result = await client.GetByCode(code);
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ AgentPortalApiGateway/RestClients/RestClientsInstaller.cs
@@ -0,0 +1,15 @@
+﻿using AgentPortalApiGateway.RestClients;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PolicyService.RestClients
+{
+    public static class RestClientsInstaller
+    {
+        public static IServiceCollection AddPricingRestClient(this IServiceCollection services)
+        {
+            services.AddSingleton(typeof(IProductClient), typeof(ProductClient));
+
+            return services;
+        }
+    }
+}
