ec0049317cdbda83e6decff71f9382816e168673
Robert Witkowski <robert.witkowski@altkomsoftware.pl>
2019-07-02 10:56:58 +0200

    Create scripts folders. Add *sh files.

---
+++ scripts/build-without-tests.sh
@@ -0,0 +1,13 @@
+dotnet build ../ProductService.Api
+dotnet build ../PricingService.Api
+dotnet build ../PolicyService.Api
+dotnet build ../PaymentService.Api
+dotnet build ../PolicySearchService.Api
+
+dotnet build ../AgentPortalApiGateway
+dotnet build ../AuthService
+dotnet build ../ProductService
+dotnet build ../PricingService
+dotnet build ../PolicyService
+dotnet build ../PaymentService
+dotnet build ../PolicySearchService
