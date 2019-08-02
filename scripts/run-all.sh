4d7e6a77f7edaa513858928215fb677366c929a6
Robert Witkowski <robert.witkowski@altkomsoftware.pl>
2019-08-02 13:28:33 +0200

    Remove not working script

--- scripts/run-all.sh
+++
@@ -1,7 +0,0 @@
-dotnet run --project "../AgentPortalApiGateway" &
-dotnet run --project "../AuthService" &
-# dotnet run ../ProductService
-# dotnet run ../PricingService
-# dotnet run ../PolicyService
-# dotnet run ../PaymentService
-# dotnet run ../PolicySearchService
