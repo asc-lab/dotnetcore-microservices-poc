36b554d236db293389c0d3ab13ee7fedd393a6da
wojteksuwala <wojtek.suwala@outlook.com>
2021-02-26 18:32:14 +0100

    Gradle and spring boot upgrade

--- eureka-server/src/main/java/net/chrisrichardson/microservices/eurekaserver/EurekaServer.java
+++ eureka-server/src/main/java/net/chrisrichardson/microservices/eurekaserver/EurekaServer.java
@@ -1,7 +1,7 @@
 package net.chrisrichardson.microservices.eurekaserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServer {
 
   public static void main(String[] args) {
-    new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
+    SpringApplication.run(EurekaServer.class, args);
   }
 
 }
