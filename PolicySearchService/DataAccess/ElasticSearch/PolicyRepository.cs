72e21fbfce92168dba86032094fcd29107128a4b
Gower <gower.tw@gmail.com>
2023-01-17 11:25:05 +0800

    Upgrade solusion (all projects) to .NET 7 Update NuGet package to latest version

--- PolicySearchService/DataAccess/ElasticSearch/PolicyRepository.cs
+++ PolicySearchService/DataAccess/ElasticSearch/PolicyRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Policy = PolicySearchService.Domain.Policy;
 
 namespace PolicySearchService.DataAccess.ElasticSearch
 {
